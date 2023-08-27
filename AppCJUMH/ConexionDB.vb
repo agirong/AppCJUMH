69c85e2bb28233b1c698fb8a0bf0fce7d94cf8be
agirong <aogarrido.@gmail.com>
2023-08-27 14:48:49 -0600

    Se creo la clase ConexionDB para manejar los metodos de acceso a la base de datos.

---
+++ AppCJUMH/ConexionDB.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data.SqlClient
+
+Public Class ConexionDB
+    Private Const ConnectionString As String = "Data Source=.;Initial Catalog=CJUMH;Integrated Security=True"
+
+    Public Shared Function GetDataTable(query As String) As DataTable
+        Using connection As New SqlConnection(ConnectionString)
+            Dim dataTable As New DataTable()
+            Using command As New SqlCommand(query, connection)
+                Dim adapter As New SqlDataAdapter(command)
+                adapter.Fill(dataTable)
+            End Using
+            Return dataTable
+        End Using
+    End Function
+
+    Public Shared Sub InsertData(consulta As String, parametros As SqlParameter())
+        Using connection As New SqlConnection(ConnectionString)
+            connection.Open()
+            Using command As New SqlCommand(consulta, connection)
+                command.Parameters.AddRange(parametros)
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+End Class
