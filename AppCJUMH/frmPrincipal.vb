8bd925299a95f0038cab36140fdf1ee7cd3e91b7
agirong <aogarrido.@gmail.com>
2023-08-26 20:16:16 -0600

    Agregar: Se ha creado un frmPrincipal para controlar el flujo dentro de la aplicacion.

---
+++ AppCJUMH/frmPrincipal.vb
@@ -0,0 +1,61 @@
+﻿Public Class frmPrincipal
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'YM. Crear una instancia del formulario que deseas mostrar
+        Dim form1 As New Form1()
+
+        'YM. Mostrar el formulario
+        form1.Show()
+
+        'YM. Opcionalmente, puedes ocultar el formulario actual
+        Me.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'YM. Crear una instancia del formulario que deseas mostrar
+        Dim form2 As New Form2()
+
+        'YM. Mostrar el formulario
+        form2.Show()
+
+        'YM. Opcionalmente, puedes ocultar el formulario actual
+        Me.Hide()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'YM. Crear una instancia del formulario que deseas mostrar
+        Dim form3 As New Form3()
+
+        'YM. Mostrar el formulario
+        form3.Show()
+
+        'YM. Opcionalmente, puedes ocultar el formulario actual
+        Me.Hide()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'YM. Crear una instancia del formulario que deseas mostrar
+        Dim form4 As New Form4()
+
+        'YM. Mostrar el formulario
+        form4.Show()
+
+        'YM. Opcionalmente, puedes ocultar el formulario actual
+        Me.Hide()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'YM. Crear una instancia del formulario que deseas mostrar
+        Dim form5 As New Form5()
+
+        'YM. Mostrar el formulario
+        form5.Show()
+
+        'YM. Opcionalmente, puedes ocultar el formulario actual
+        Me.Hide()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+End Class
