1540450772a89fe5470fe3e08fbc64f20078ef32
agirong <aogarrido.@gmail.com>
2023-09-03 08:35:54 -0600

    Avance: Se creo la pantalla de login con sus componentes usuario, contrasena, btnLogin

---
+++ AppCJUMH/frmLogin.vb
@@ -0,0 +1,20 @@
+﻿Public Class frmLogin
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnIngresarLogin_Click(sender As Object, e As EventArgs) Handles btnIngresarLogin.Click
+        Try
+            If txtUsuarioLogin.Text = "" Then
+                MessageBox.Show("Ingrese el Usuario")
+            ElseIf txtContrasenaLogin.Text = "" Then
+                MessageBox.Show("Ingrese la Contrasena")
+            Else
+
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
